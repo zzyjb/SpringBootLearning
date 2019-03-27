ef8d44f5111ffa9050b5707d66dcd358264620c8
zzyjb <zzyjb@163.com>
2019-03-27 10:51:31 +0800

    Spring Boot 学习

---
+++ src/main/java/com/lanxum/App.java
@@ -0,0 +1,13 @@
+package com.lanxum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
