b04f5d89add10f17cb7a9af62bbcf6aebb5426ba
zzyjb <zzyjb@163.com>
2019-03-29 17:18:23 +0800

    spring boot 基于DeferredResult的异步HTTP服务

---
+++ spring-boot-async-servlet/src/test/java/com/picea/springbootasyncservlet/SpringBootAsyncServletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.picea.springbootasyncservlet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAsyncServletApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
