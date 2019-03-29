c4e2e2ba1f4763a647a8f4b5fb49983aa1a58ad8
zzyjb <zzyjb@163.com>
2019-03-29 16:23:30 +0800

    spring boot 基于DeferredResult的异步HTTP服务

---
+++ spring-boot-async-deferredresult/src/main/java/com/picea/SpringBootAsyncDeferredresultApplication.java
@@ -0,0 +1,13 @@
+package com.picea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsyncDeferredresultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAsyncDeferredresultApplication.class, args);
+    }
+
+}
